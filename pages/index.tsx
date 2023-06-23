3121f255e149433d48d7d76336f3d6b03bb2fb2b
龍田 佳樹 <yo-tatsuta@a.tgl.co.jp>
2023-06-23 14:06:15 +0900

    20230623

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
     };
     return (
         <div className={styles.page}>
-            <button onClick={handleClick} className={styles.button}>他のにゃんこも見る</button>
+            <button onClick={handleClick} className={styles.button}>他のにゃんこも見る🐱</button>
             <div className={styles.frame}>
                 {loading || <img src={imageUrl} className={styles.img} />}
             </div>
